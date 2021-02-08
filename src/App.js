76dd1a7f87ecc9c900bdf36bbd1781bb30820a8a
rabobahago <rabobahago@gmail.com>
2021-02-08 16:09:40 +0100

    react router dom and it codes added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
+import Footer from './components/Footer'
+import About from './components/About'
 import './App.css'
 
 const App = () => {
@@ -63,22 +66,35 @@ const App = () => {
   }
 
   return (
-    <div className="container">
-      <Header
-        onAdd={() => setShowAddTask(!showAddTask)}
-        showAdd={showAddTask}
-      />
-      {showAddTask && <AddTask onAdd={addTask} />}
-      {tasks.length > 0 ? (
-        <Tasks
-          tasks={tasks}
-          deleteTask={deleteTask}
-          onToggle={toggleReminder}
+    <Router>
+      <div className="container">
+        <Header
+          onAdd={() => setShowAddTask(!showAddTask)}
+          showAdd={showAddTask}
         />
-      ) : (
-        'No task to be deleted'
-      )}
-    </div>
+
+        <Route
+          path="/"
+          exact
+          render={(props) => (
+            <>
+              {showAddTask && <AddTask onAdd={addTask} />}
+              {tasks.length > 0 ? (
+                <Tasks
+                  tasks={tasks}
+                  deleteTask={deleteTask}
+                  onToggle={toggleReminder}
+                />
+              ) : (
+                'No task to be deleted'
+              )}
+            </>
+          )}
+        />
+        <Route path="/about" component={About} />
+        <Footer />
+      </div>
+    </Router>
   )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,18 @@
 import PropTypes from 'prop-types'
 import Button from './Button'
+import { useLocation } from 'react-router-dom'
 const Header = ({ title, onAdd, showAdd }) => {
+  const location = useLocation()
   return (
     <header className="header">
       <h1>{title}</h1>
-      <Button
-        onAdd={onAdd}
-        color={showAdd ? 'red' : 'green'}
-        text={showAdd ? 'Close' : 'Add'}
-      />
+      {location.pathname === '/' && (
+        <Button
+          onAdd={onAdd}
+          color={showAdd ? 'red' : 'green'}
+          text={showAdd ? 'Close' : 'Add'}
+        />
+      )}
     </header>
   )
 }
